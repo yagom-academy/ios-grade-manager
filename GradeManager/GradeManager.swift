f730b81fb069511333e03f3707cdf53426aeb9a6
Jinwook Huh <gjwlsdnr0115@Jinwooks-MacBook-Pro.local>
2021-12-27 20:21:42 +0900

    refactor(GradeManager): Change variable naming.

--- GradeManager/GradeManager.swift
+++ GradeManager/GradeManager.swift
@@ -11,10 +11,10 @@ class GradeManager {
     
     private var isDone = false
     
-    let menuInputPromptText = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
-    let invalidMenuInputAlertPromptText = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
-    let closeProgramPromptText = "프로그램을 종료합니다..."
-    let addStudentPromptText = "추가할 학생의 이름을 입력해주세요"
+    let menuInputPrompt = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
+    let invalidMenuInputPrompt = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
+    let closeProgramPrompt = "프로그램을 종료합니다..."
+    let addStudentPrompt = "추가할 학생의 이름을 입력해주세요"
     let invalidInputPrompt = "입력이 잘못되었습니다. 다시 확인해주세요."
     
     
@@ -26,7 +26,7 @@ class GradeManager {
     
     
     private func receiveMenuInput() {
-        print(menuInputPromptText)
+        print(menuInputPrompt)
         
         let receivedInput = readLine()
         
@@ -45,16 +45,16 @@ class GradeManager {
             stopProgram()
             
         default:
-            print(invalidInputAlertPromptText)
+            print(invalidMenuInputPrompt)
         }
     }
     
     private func addStudent() {
-        print(addStudentPromptText)
+        print(addStudentPrompt)
         let receivedInput = readLine()
         
         guard let receivedInput = receivedInput else {
-            print(<#T##items: Any...##Any#>)
+            print(invalidInputPrompt)
             return
         }
 
@@ -82,7 +82,7 @@ class GradeManager {
     
     private func stopProgram() {
         isDone = true
-        print(closeProgramPromptText)
+        print(closeProgramPrompt)
     }
     
     
