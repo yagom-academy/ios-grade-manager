f40b89575e3b32c54802bb7a28285693908f2769
Jinwook Huh <gjwlsdnr0115@Jinwooks-MacBook-Pro.local>
2021-12-27 18:01:34 +0900

    feat: Add menu function in GradeManager class

--- GradeManager/GradeManager.swift
+++ GradeManager/GradeManager.swift
@@ -9,12 +9,76 @@ import Foundation
 
 class GradeManager {
     
-    var shouldTerminate = false
+    private var endProgramFlag = false
+    
+    var isDone: Bool {
+        get {
+            return endProgramFlag
+        }
+    }
     
     let menuInputPromptText = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
+    let invalidInputAlertPromptText = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
     
-    func printMenuInputPrompt() {
+    
+    
+    
+    private func printMenuInputPrompt() {
         print(menuInputPromptText)
     }
+    
+    func receiveMenuInput() {
+        printMenuInputPrompt()
+        
+        let receivedInput = readLine()
+        
+        switch receivedInput {
+        case "1":
+            addStudent()
+        case "2":
+            removeStudent()
+        case "3":
+            addOrModifyGrade()
+        case "4":
+            removeGrade()
+        case "5":
+            lookupGrade()
+        case "X":
+            stopProgram()
+            
+        default:
+            print(invalidInputAlertPromptText)
+        }
+    }
+    
+    private func addStudent() {
+        print(#function)
+    }
+    
+    private func removeStudent() {
+        print(#function)
+
+    }
+    
+    private func addOrModifyGrade() {
+        print(#function)
+
+    }
+    
+    private  func removeGrade() {
+        print(#function)
+
+    }
+    
+    private func lookupGrade() {
+        print(#function)
+
+    }
+    
+    private func stopProgram() {
+        endProgramFlag = true
+    }
+    
+    
     
 }
--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -7,3 +7,8 @@
 import Foundation
 
 let gradeManager = GradeManager()
+
+
+while !gradeManager.isDone {
+    gradeManager.receiveMenuInput()
+}
