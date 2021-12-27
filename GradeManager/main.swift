298233073c66114c89081c727058e52640290aed
claire-pp <claire.pp@kakaocorp.com>
2021-12-28 00:09:27 +0900

    feat: InputManager 클래스 추가, readUserInput, toStudent 구현
    사용자 입력을 관리하는 클래스인 InputManager를 추가함
    InputManagable 프로토콜을 채택하므로,
    입력을 받는 readUserInput 메서드와
    입력을 통해 Student 객체를 생성하는 toStudent 메서드를 구현함

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -16,3 +16,21 @@ protocol MenuManagable {
 struct Student {
     let name: String
 }
+
+class InputManager: InputManagable {
+    func readUserInput() -> String? {
+        let userInput: String? = readLine()
+        if userInput == nil || userInput == "" {
+            return nil
+        }
+        return userInput
+    }
+    
+    func toStudent() -> Student? {
+        let name: String? = readUserInput()
+        if name == nil {
+            return nil
+        }
+        return Student(name: name ?? "")
+    }
+}
