faf32f2f309168cd5bfcff9e6202f873ff7d0320
cms7371 <cms7371@gmail.com>
2021-12-28 13:37:28 +0900

    feat: 프로그램 상태 및 컨트롤러 정의, 메인 메뉴 동작 완성
    - 프로그램 상태에 따른 6가지 enum ProgressState 정의
    - ProgressState에 따라 프로그램 동작을 책임지는 ProgressController 정의
    - 메인 메뉴 동작을 위해 입력에 따라 ProgressState를 반환하는 메서드 정의

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -5,3 +5,68 @@
 // 
 
 import Foundation
+
+
+
+class ProgressController {
+    func run() {
+        var currentState: ProgressState = .mainMenu
+        mainLoop: while true {
+            print(currentState.rawValue)
+            switch currentState {
+            case .mainMenu:
+                currentState = ProgressState.makeFromInput()
+                continue mainLoop
+            case .addStudent:
+                break
+            case .deleteStudent:
+                break
+            case .addOrChangeGrade:
+                break
+            case .deleteGrade:
+                break
+            case .checkGrades:
+                break
+            case .terminateProgram:
+                break mainLoop
+            }
+            currentState = .mainMenu
+        }
+    }
+    
+}
+
+enum ProgressState: String {
+    case mainMenu = """
+    원하는 기능을 입력해주세요
+    1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 병점보기, X: 종료
+    """
+    case addStudent = "추가할 학생의 이름을 입력해주세요"
+    case deleteStudent
+    case addOrChangeGrade
+    case deleteGrade
+    case checkGrades
+    case terminateProgram = "프로그램을 종료합니다..."
+    
+    static func makeFromInput() -> ProgressState {
+        let input: String? = readLine()
+        switch input {
+        case "1":
+            return addStudent
+        case "2":
+            return deleteStudent
+        case "3":
+            return addOrChangeGrade
+        case "4":
+            return deleteGrade
+        case "5":
+            return checkGrades
+        case "X":
+            return terminateProgram
+        default:
+            print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요")
+            return mainMenu
+        }
+    }
+}
+
