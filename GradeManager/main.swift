ae3cd25c0b158f986d41c2ec9ca40b043d25fd11
claire-pp <claire.pp@kakaocorp.com>
2021-12-28 14:57:10 +0900

    feat: [수정] toStudent, Command, Behavior, add, delete, run
    toStudent 함수에 메시지를 매개변수로 받아 출력하도록 수정
    Command, Behavior의 값 수정
    add, delete에 존재하는 학생에 따른 동작 추가, 옵셔널 매개변수
    메뉴를 계속해서 출력해주고 입력을 받는 run 함수 추가
    run함수에 입력에 따른 메뉴를 연결하는 부분을 inputToMenu로 분리

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -2,15 +2,15 @@ import Foundation
 
 protocol InputManagable {
     func readUserInput() -> String?
-    func toStudent() -> Student?
+    func toStudent(message: String) -> Student?
     func toMenu() -> String?
 }
 
 protocol MenuManagable {
     var students: Set<Student> { get set }
     func run() -> Bool
-    func add(student: Student)
-    func delete(student: Student)
+    func add(student: Student?)
+    func delete(student: Student?)
     func find(name: String) -> Student?
 }
 
@@ -30,7 +30,8 @@ class InputManager: InputManagable {
         return userInput
     }
     
-    func toStudent() -> Student? {
+    func toStudent(message: String) -> Student? {
+        print(message)
         guard let name: String = readUserInput() else { return nil }
         return Student(name: name)
     }
@@ -44,7 +45,7 @@ class InputManager: InputManagable {
 
 class MenuManager: MenuManagable {
     enum Command: String, CaseIterable {
-        case studentAdd = "1", studentDelete = "2", gradeAdd = "3", gradeDelete = "4", show = "5", end = "x"
+        case studentAdd = "1", studentDelete = "2", gradeAdd = "3", gradeDelete = "4", show = "5", end = "X"
         static var range: ClosedRange<Int> {
             let numbers = Self.allCases.compactMap({Int.init($0.rawValue)})
             return (numbers.min() ?? 0)...(numbers.max() ?? 0)
@@ -56,7 +57,7 @@ class MenuManager: MenuManagable {
     }
     
     enum Behavior {
-        case menu, wrongMenu, needName, wrongInput, nameExist(String), addedStudent(String), deletedStudent(String), programEnd
+        case menu, wrongMenu, needAddName, needDeleteName, wrongInput, nameExist(Student), noStudent(Student), addedStudent(Student), deletedStudent(Student), programEnd
         
         var output: String {
             switch self {
@@ -64,18 +65,22 @@ class MenuManager: MenuManagable {
                 return "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
             case .wrongMenu:
                 return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요"
-            case .needName:
+            case .needAddName:
                 return "추가할 학생의 이름을 입력해주세요"
+            case .needDeleteName:
+                return "삭제할 학생의 이름을 입력해주세요"
             case .wrongInput:
                 return "입력이 잘못되었습니다. 다시 확인해주세요"
-            case .nameExist(let name):
-                return "\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다."
-            case .addedStudent(let name):
-                return "\(name) 학생을 추가했습니다."
-            case .deletedStudent(let name):
-                return "\(name) 학생을 삭제했습니다."
+            case .nameExist(let student):
+                return "\(student.name) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다."
+            case .addedStudent(let student):
+                return "\(student.name) 학생을 추가했습니다."
+            case .noStudent(let student):
+                return "\(student.name) 학생을 찾지 못했습니다."
+            case .deletedStudent(let student):
+                return "\(student.name) 학생을 삭제했습니다."
             case .programEnd:
-                return "[프로그램 종료]"
+                return "프로그램을 종료합니다"
             }
         }
     }
@@ -88,13 +93,46 @@ class MenuManager: MenuManagable {
         self.students = Set<Student>()
     }
 
-    func run() -> Bool { return true }
+    func run() -> Bool {
+        print(Behavior.menu.output)
+        guard let userInput = inputManager.toMenu() else { print(Behavior.wrongMenu.output); return true }
+        inputToMenu(input: userInput)
+        if userInput == Command.end.rawValue {
+            return false
+        }
+        return true
+    }
     
-    func add(student: Student) {
+    func inputToMenu(input: String) {
+        switch input {
+        case Command.studentAdd.rawValue:
+            add(student: inputManager.toStudent(message: Behavior.needAddName.output))
+        case Command.studentDelete.rawValue:
+            delete(student: inputManager.toStudent(message: Behavior.needDeleteName.output))
+        case Command.end.rawValue:
+            print(Behavior.programEnd.output)
+        default:
+            print(Behavior.wrongMenu.output)
+        }
+    }
+    
+    func add(student: Student?) {
+        guard let student: Student = student else { print(Behavior.wrongInput.output); return }
+        if students.contains(student) {
+            print(Behavior.nameExist(student).output)
+            return
+        }
+        print(Behavior.addedStudent(student).output)
         students.insert(student)
     }
     
-    func delete(student: Student) {
+    func delete(student: Student?) {
+        guard let student: Student = student else { print(Behavior.wrongInput.output); return }
+        if !students.contains(student){
+            print(Behavior.noStudent(student).output)
+            return
+        }
+        print(Behavior.deletedStudent(student).output)
         students.remove(student)
     }
     
