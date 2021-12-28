327694d2c07f274891fc9bc208f0ff58edf0c8b2
gustn3965 <gustn3965@gmail.com>
2021-12-28 10:48:07 +0900

    feat: Step1 - 학생추가 구현

---
+++ GradeManager/Storage.swift
@@ -0,0 +1,21 @@
+//
+//  Storage.swift
+//  GradeManager
+//
+//  Created by kakao on 2021/12/28.
+//
+
+import Foundation
+
+struct Storage {
+    private var storage: [String: Int] = [:]
+    
+    mutating func addStudent(_ student: String) {
+        storage[student] = 5
+    }
+    
+    func list() -> [String] {
+        let students = storage.keys.sorted(by: <)
+        return students
+    }
+}
