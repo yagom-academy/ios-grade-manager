fa03908ac9c8ab44033012eb1be8053aa048919d
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-12-28 17:05:45 +0900

    feat: Step1 메뉴 출력 및 메뉴의 잘못된 입력 처리, 학생 추가, 종료, 학생 삭제 구현

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -5,3 +5,95 @@
 // 
 
 import Foundation
+
+enum Menu: String {
+    case addStudent = "1"
+    case deleteStudent = "2"
+    case exit = "X"
+}
+
+struct Student: Hashable {
+    let name: String
+}
+
+struct GradeManager {
+    private var students = Set<Student>()
+    
+    mutating func selectMenu(_ menu: String) -> Bool {
+        switch Menu(rawValue: menu) {
+        case .addStudent:
+            print("추가할 학생의 이름을 입력해주세요")
+            guard let name = readLine(), verifyInput(name) else {
+                print("입력이 잘못되었습니다. 다시 확인해주세요.")
+                return false
+            }
+            
+            let student = Student(name: name)
+            _ = addStudent(student)
+            return false
+        case .deleteStudent:
+            print("삭제할 학생의 이름을 입력해주세요")
+            guard let name = readLine(), verifyInput(name) else {
+                print("입력이 잘못되었습니다. 다시 확인해주세요.")
+                return false
+            }
+            
+            let student = Student(name: name)
+            _ = deleteStudent(student)
+            return false
+        case .exit:
+            return exit()
+        default:
+            return isInvaildMenuFormat()
+        }
+    }
+    
+    mutating func addStudent(_ student: Student) -> Bool {
+        if students.contains(student) == false {
+            print("\(student.name) 학생을 추가했습니다.")
+            students.insert(student)
+            return true
+        }
+        
+        print("\(student.name) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다.")
+        return false
+    }
+    
+    mutating func deleteStudent(_ student: Student) -> Bool {
+        if students.contains(student) == true {
+            print("\(student.name) 학생을 삭제하였습니다.")
+            students.remove(student)
+            return true
+        }
+        
+        print("\(student.name) 학생을 찾지 못했습니다.")
+        return false
+    }
+    
+    private func exit() -> Bool {
+        print("프로그램을 종료합니다...")
+        return true
+    }
+
+    private func isInvaildMenuFormat() -> Bool {
+        print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요")
+        return false
+    }
+    
+    private func verifyInput(_ input: String) -> Bool {
+        let pattern = "[^a-zA-Z0-9]"
+        return input.isEmpty == false
+            && input.range(of: pattern, options: .regularExpression) == nil
+    }
+}
+
+var gradeManager = GradeManager()
+var isExit = false
+
+repeat {
+    print("원하는 기능을 입력해주세요")
+    print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+    if let menu = readLine() {
+        isExit = gradeManager.selectMenu(menu)
+    }
+} while isExit == false
---
+++ GradeManagerTests/GradeManagerTests.swift
@@ -0,0 +1,67 @@
+//
+//  GradeManagerTests.swift
+//  GradeManagerTests
+//
+//  Created by 이승주 on 2021/12/28.
+//
+
+import XCTest
+@testable import GradeManager
+
+final class GradeManagerTests: XCTestCase {
+    var gradeManager =  GradeManager()
+    
+    func test_학생추가_성공() {
+        let student = Student(name: "joel")
+        
+        let result = gradeManager.addStudent(student)
+        
+        XCTAssertEqual(result, true)
+    }
+    
+    func test_학생추가_중복추가실패() {
+        let student1 = Student(name: "joel")
+        let student2 = Student(name: "joel")
+        
+        var result = gradeManager.addStudent(student1)
+        result = gradeManager.addStudent(student2)
+        
+        XCTAssertEqual(result, false)
+    }
+    
+    func test_학생삭제_성공() {
+        let student = Student(name: "joel")
+        _ = gradeManager.addStudent(student)
+        
+        let result = gradeManager.deleteStudent(student)
+        
+        XCTAssertEqual(result, true)
+    }
+    
+    func test_학생삭제_실패() {
+        let student1 = Student(name: "joel")
+        let student2 = Student(name: "joel2")
+        _ = gradeManager.addStudent(student1)
+        
+        let result = gradeManager.deleteStudent(student2)
+        
+        XCTAssertEqual(result, false)
+    }
+    
+    func test_종료() {
+        let input = "X"
+        
+        let result = gradeManager.selectMenu(input)
+        
+        XCTAssertEqual(result, true)
+    }
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+}
