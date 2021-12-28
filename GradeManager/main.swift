0794df033e98ff8f748c53632679e336c9688eae
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-12-28 17:24:51 +0900

    refactor: main 함수에서 메뉴 입력을 처리하도록 변경

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -87,13 +87,17 @@ struct GradeManager {
     }
 }
 
-var gradeManager = GradeManager()
-var isExit = false
+func main() {
+    var gradeManager = GradeManager()
+    var isExit = false
 
-repeat {
-    print("원하는 기능을 입력해주세요")
-    print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
-    if let menu = readLine() {
-        isExit = gradeManager.selectMenu(menu)
-    }
-} while isExit == false
+    repeat {
+        print("원하는 기능을 입력해주세요")
+        print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+        if let menu = readLine() {
+            isExit = gradeManager.selectMenu(menu)
+        }
+    } while isExit == false
+}
+
+main()
