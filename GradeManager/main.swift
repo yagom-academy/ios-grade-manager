b0817f39d2243f4193afa353af4eb50fefac1e98
claire-pp <claire.pp@kakaocorp.com>
2021-12-28 14:59:47 +0900

    feat: [추가] 메인문
    구현된 클래스를 바탕으로 메인문 작성

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -144,3 +144,9 @@ class MenuManager: MenuManagable {
         return filtered[0]
     }
 }
+
+let inputManager: InputManager = InputManager()
+let menuManager: MenuManager = MenuManager(inputManager: inputManager)
+while menuManager.run() {
+    print()
+}
