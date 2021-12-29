f817beeec6e01b796c78c3b3602d3518edffca71
claire-pp <claire.pp@kakaocorp.com>
2021-12-29 16:07:51 +0900

    chore: [수정] 개행, 열거형 Behavior와 Error 분리
    가독성을 위해 몇가지 구문을 개행했고,
    열거형 Behavior를 목적에 따라 안내 메시지 Behavior와
    에러 메시지 출력을 위해 필요한 Error로 분리함

--- GradeManager/main.swift
+++ GradeManager/main.swift
@@ -44,7 +44,10 @@ class InputManager: InputManagable {
 
 class MenuManager: MenuManagable {
     enum Command: String, CaseIterable {
-        case studentAdd = "1", studentDelete = "2", gradeAdd = "3", gradeDelete = "4", show = "5", end = "X"
+        case studentAdd = "1", studentDelete = "2",
+             gradeAdd = "3", gradeDelete = "4",
+             show = "5", end = "X"
+        
         static var range: ClosedRange<Int> {
             let numbers = Self.allCases.compactMap({Int.init($0.rawValue)})
             return (numbers.min() ?? 0)...(numbers.max() ?? 0)
@@ -56,30 +59,42 @@ class MenuManager: MenuManagable {
     }
     
     enum Behavior {
-        case menu, wrongMenu, needAddName, needDeleteName, wrongInput, nameExist(Student), noStudent(Student), addedStudent(Student), deletedStudent(Student), programEnd
+        case menu,
+             needAddName, needDeleteName,
+             addedStudent(Student), deletedStudent(Student),
+             programEnd
         
         var output: String {
             switch self {
             case .menu:
                 return "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
-            case .wrongMenu:
-                return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요"
             case .needAddName:
                 return "추가할 학생의 이름을 입력해주세요"
             case .needDeleteName:
                 return "삭제할 학생의 이름을 입력해주세요"
-            case .wrongInput:
-                return "입력이 잘못되었습니다. 다시 확인해주세요"
-            case .nameExist(let student):
-                return "\(student.name) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다."
             case .addedStudent(let student):
                 return "\(student.name) 학생을 추가했습니다."
-            case .noStudent(let student):
-                return "\(student.name) 학생을 찾지 못했습니다."
             case .deletedStudent(let student):
                 return "\(student.name) 학생을 삭제했습니다."
             case .programEnd:
                 return "프로그램을 종료합니다"
+            }
+        }
+    }
+    
+    enum Error {
+        case wrongMenu, wrongInput, nameExist(Student), noStudent(Student)
+        
+        var output: String {
+            switch self {
+            case .wrongMenu:
+                return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요"
+            case .wrongInput:
+                return "입력이 잘못되었습니다. 다시 확인해주세요"
+            case .nameExist(let student):
+                return "\(student.name) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다."
+            case .noStudent(let student):
+                return "\(student.name) 학생을 찾지 못했습니다."
             }
         }
     }
@@ -94,7 +109,7 @@ class MenuManager: MenuManagable {
 
     func run() -> Bool {
         print(Behavior.menu.output)
-        guard let userInput = inputManager.toMenu() else { print(Behavior.wrongMenu.output); return true }
+        guard let userInput = inputManager.toMenu() else { print(Error.wrongMenu.output); return true }
         inputToMenu(input: userInput)
         return userInput != Command.end.rawValue
     }
@@ -110,14 +125,14 @@ class MenuManager: MenuManagable {
         case Command.gradeAdd.rawValue, Command.gradeDelete.rawValue, Command.show.rawValue:
             print("아직 준비되지 않은 기능입니다.")
         default:
-            print(Behavior.wrongMenu.output)
+            print(Error.wrongMenu.output)
         }
     }
     
     func add(student: Student?) {
-        guard let student: Student = student else { print(Behavior.wrongInput.output); return }
+        guard let student: Student = student else { print(Error.wrongInput.output); return }
         if students.contains(student) {
-            print(Behavior.nameExist(student).output)
+            print(Error.nameExist(student).output)
             return
         }
         students.insert(student)
@@ -125,13 +140,13 @@ class MenuManager: MenuManagable {
     }
     
     func delete(student: Student?) {
-        guard let student: Student = student else { print(Behavior.wrongInput.output); return }
+        guard let student: Student = student else { print(Error.wrongInput.output); return }
         if students.contains(student){
             students.remove(student)
             print(Behavior.deletedStudent(student).output)
             return
         }
-        print(Behavior.noStudent(student).output)
+        print(Error.noStudent(student).output)
     }
 }
 
