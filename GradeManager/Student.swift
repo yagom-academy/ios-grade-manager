556868b800a7ba99266372c5bf0181d8fb443a7a
claire-pp <claire.pp@kakaocorp.com>
2021-12-30 01:22:47 +0900

    feat: [수정] Student 구조체 Hashable 속성 삭제, grades 프로퍼티 추가
    MenuManager에서 Set을 사용하지 않음에 따라 Hashable 속성을 삭제함
    또한, 학생 성적에 대한 프로퍼티인 grades 프로퍼티 추가

--- GradeManager/Student.swift
+++ GradeManager/Student.swift
@@ -2,10 +2,5 @@ import Foundation
 
 struct Student {
     let name: String
-}
-
-extension Student: Hashable {
-    static func == (lhs: Student, rhs: Student) -> Bool {
-        return lhs.name == rhs.name
-    }
+    var grades: Dictionary<String, String> = [String: String]()
 }
