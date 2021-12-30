e0da8e1135d81adff5f88b9738537604e3f3d2d9
hayden.chae <cms7371@gmail.com>
2021-12-30 11:28:24 +0900

    refactor: Step 2 성적 저장 구조 변경
    - 이중 딕셔너리에서 명시적인 클래스로 변경

--- GradeManager/StudentDictionary.swift
+++ GradeManager/StudentDictionary.swift
@@ -9,21 +9,21 @@ import Foundation
 
 struct StudentDictionary {
     private init() { }
-    private var nameForSubjects = [String: [String: String]]()
+    private var nameForSubjectInformation = [String: SubjectDictionary]()
     
     static var instance = StudentDictionary()
     
     mutating func addStudent(name: String) {
-        guard nameForSubjects[name] == nil else {
+        guard nameForSubjectInformation[name] == nil else {
             print("\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
             return
         }
-        nameForSubjects[name] = [String: String]()
+        nameForSubjectInformation[name] = SubjectDictionary()
         print("\(name) 학생을 추가했습니다.")
     }
     
     mutating func removeStudent(forName name: String) {
-        guard let _ = nameForSubjects.removeValue(forKey: name) else {
+        guard let _ = nameForSubjectInformation.removeValue(forKey: name) else {
             print("\(name) 학생을 찾지 못했습니다.")
             return
         }
@@ -31,11 +31,11 @@ struct StudentDictionary {
     }
     
     mutating func addOrChangeGrade(name: String, subject: String, grade: String) {
-        guard nameForSubjects[name] != nil else {
+        guard let subjectForGrade = nameForSubjectInformation[name] else {
             print("\(name) 학생을 찾지 못했습니다")
             return
         }
-        nameForSubjects[name]?[subject] = grade
+        subjectForGrade[subject] = grade
         print("\(name) 학생의 \(subject) 과목이 \(grade)로 추가(변경)되었습니다.")
     }
 }
---
+++ GradeManager/SubjectDictionary.swift
@@ -0,0 +1,21 @@
+//
+//  SubjectDictionary.swift
+//  GradeManager
+//
+//  Created by kakao on 2021/12/30.
+//
+
+import Foundation
+
+class SubjectDictionary {
+    var subjectForGrade = [String: String]()
+    
+    subscript(key: String) -> String? {
+        get {
+            return subjectForGrade[key]
+        }
+        set(newValue) {
+            subjectForGrade[key] = newValue
+        }
+    }
+}
