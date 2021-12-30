74b43fc8d9534f08695d68293bae932bff42fdf1
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-12-30 16:47:39 +0900

    refactor: PrintManager 추가, Constants 변수 네이밍 수정, calculateGPA 메서드 코드 수정

--- GradeManager/GradeManager.swift
+++ GradeManager/GradeManager.swift
@@ -8,6 +8,7 @@
 struct GradeManager {
     private var students = [String: Student]()
     private let verificationManager = VerificationManager()
+    private let printManager = PrintManager()
     
     mutating func selectMenu(_ menu: String) -> Bool {
         switch Menu(rawValue: menu) {
@@ -15,27 +16,27 @@ struct GradeManager {
             print(K.explanatoryTextForAddStudent)
             
             guard let name = readLine(), verificationManager.verifyStudentName(name) else {
-                print(K.explanatoryTextForInvaildInput)
+                printManager.explanatoryTextForInvaildInput()
                 return false
             }
             
             _ = addStudent(name)
             return false
         case .deleteStudent:
-            print(K.explanatoryTextForDeleteStudent)
+            printManager.explanatoryTextForDeleteStudent()
             
             guard let name = readLine(), verificationManager.verifyStudentName(name) else {
-                print(K.explanatoryTextForInvaildInput)
+                printManager.explanatoryTextForInvaildInput()
                 return false
             }
             
             _ = deleteStudent(name)
             return false
         case .addGradeForSubject:
-            print(K.explanatoryTextForAddGradeForSubject)
+            printManager.explanatoryTextForAddGradeForSubject()
             
             guard let gradeInfo = readLine(), verificationManager.verifyAdditinalGradeInfo(gradeInfo) else {
-                print(K.explanatoryTextForInvaildInput)
+                printManager.explanatoryTextForInvaildInput()
                 return false
             }
             
@@ -50,10 +51,10 @@ struct GradeManager {
             _ = addGradeForSubject(name, subject, grade)
             return false
         case .deleteGradeForSubject:
-            print(K.explanatoryTextForDeleteGradeForSubject)
+            printManager.explanatoryTextForDeleteGradeForSubject()
             
             guard let gradeInfo = readLine(), verificationManager.verifyDeletionGradeInfo(gradeInfo) else {
-                print(K.explanatoryTextForInvaildInput)
+                printManager.explanatoryTextForInvaildInput()
                 return false
             }
             
@@ -67,10 +68,10 @@ struct GradeManager {
             _ = deleteGradeForSubject(name, subject)
             return false
         case .calculateGPA:
-            print(K.explanatoryTextForCalculateGPA)
+            printManager.explanatoryTextForCalculateGPA()
             
             guard let name = readLine(), verificationManager.verifyStudentName(name) else {
-                print(K.explanatoryTextForInvaildInput)
+                printManager.explanatoryTextForInvaildInput()
                 return false
             }
             
@@ -86,75 +87,76 @@ struct GradeManager {
     mutating func addStudent(_ name: String) -> Bool {
         if students[name] == nil {
             students.updateValue(Student(name: name), forKey: name)
-            print("\(name) 학생을 추가했습니다.")
+            printManager.printSuccessAddStudent(name)
             return true
         }
         
-        print("\(name) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다.")
+        printManager.printAlreadyExistStudent(name)
         return false
     }
     
     mutating func deleteStudent(_ name: String) -> Bool {
         if students[name] == nil {
-            print("\(name) 학생을 찾지 못했습니다.")
+            printManager.printDoNotExistStudent(name)
             return false
         }
         
         students[name] = nil
-        print("\(name) 학생을 삭제하였습니다.")
+        printManager.printSuccessDeleteStudent(name)
         return true
     }
     
     mutating func addGradeForSubject(_ name: String, _ subject: String, _ grade: String) -> Bool {
         guard var student = students[name] else {
-            print("\(name) 학생을 찾지 못했습니다.")
+            printManager.printDoNotExistStudent(name)
             return false
         }
         
         student.gradeForSubject.updateValue(grade, forKey: subject)
         students[name] = student
-        print("\(name) 학생의 \(subject) 과목이 \(grade)로 추가(변경) 되었습니다.")
+        printManager.printInsertOrUpdateGrade(name, subject, grade)
         return true
     }
     
     mutating func deleteGradeForSubject(_ name: String, _ subject: String) -> Bool {
         guard var student = students[name] else {
-            print("\(name) 학생을 찾지 못했습니다.")
+            printManager.printDoNotExistStudent(name)
             return false
         }
         
         if student.gradeForSubject[subject] == nil {
-            print("\(subject) 과목의 성적을 찾지 못했습니다.")
+            printManager.printDoNotExistGradeForSubject(subject)
             return false
         }
        
         student.gradeForSubject.removeValue(forKey: subject)
-        print("\(name) 학생의 \(subject) 과목의 성적이 삭제되었습니다.")
+        printManager.printSuccessDeleteGrade(name, subject)
         return true
     }
     
     func calculateGPA(_ name: String) -> Bool {
         guard let student = students[name] else {
-            print("\(name) 학생을 찾지 못했습니다.")
+            printManager.printDoNotExistStudent(name)
             return false
         }
         
         let gradeForSubject = student.gradeForSubject
         let subjectCount = gradeForSubject.count
-        print(name)
+        
         if subjectCount == 0 {
-           print("평점 : 0.0")
+            printManager.printGPA(name, 0.0)
             return true
         }
         
-        var GPA = gradeForSubject
+        let gradesSum = gradeForSubject
             .compactMap{
                 print("\($0.key): \($0.value)")
                 return K.grades[$0.value]
             }
-            .reduce(0.0) { $0 + $1 }
-        GPA /= Double(subjectCount)
-        print("평점 : \(GPA)")
+            .reduce(0.0, +)
+        
+        let GPA = gradesSum / Double(subjectCount)
+        printManager.printGPA(name, GPA)
         return true
     }
     
