345583f312f9ec338821e0e7f1ed4d3fca4bee21
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-12-30 11:32:09 +0900

    style: Constants파일 들여쓰기 수정

--- GradeManager/Constants.swift
+++ GradeManager/Constants.swift
@@ -6,15 +6,17 @@
 //
 
 struct K {
-    static let explanatoryTextForPlayConsole = """
-원하는 기능을 입력해주세요
-1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
-"""
-    static let explanatoryTextForAddGradeForSubject = """
-성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A0, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.
-입력예) Mickey Swift A+
-만약 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.
-"""
+    static let explanatoryTextForPlayConsole
+    = """
+    원하는 기능을 입력해주세요
+    1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
+    """
+    static let explanatoryTextForAddGradeForSubject
+    = """
+    성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A0, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.
+    입력예) Mickey Swift A+
+    만약 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.
+    """
     static let explanatoryTextForAddStudent = "추가할 학생의 이름을 입력해주세요"
     static let explanatoryTextForDeleteStudent = "삭제할 학생의 이름을 입력해주세요"
     static let explanatoryTextForDeleteGradeForSubject = "성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요."
