6e8edcd5f4c649317b83fc5d5f42dfa01c244ed8
john.lim <john.lim@kakaocorp.com>
2022-12-27 16:17:27 +0900

    feat: 학생 추가 문구와 예외 문구 추가

--- GradeManager/MessageTemplate.swift
+++ GradeManager/MessageTemplate.swift
@@ -7,18 +7,30 @@
 
 import Foundation
 
-enum MessageTemplate: String, CustomStringConvertible {
-    case selectMenu =
-    """
-    원하는 기능을 입력해주세요
-    1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
-    """
-    case wrongMenu = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요"
-    
-    case exitProgram = "프로그램을 종료합니다..."
+enum MessageTemplate: CustomStringConvertible {
+    case selectMenu
+    case wrongMenu
+    case inputStudent
+    case duplicatedStudent(name: String)
+    case exitProgram
     
     var description: String {
-        return self.rawValue
+        switch self {
+        case .selectMenu:
+            return
+        """
+        원하는 기능을 입력해주세요
+        1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
+        """
+        case .wrongMenu:
+            return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요"
+        case .inputStudent:
+            return "추가할 학생의 이름을 입력해주세요"
+        case .duplicatedStudent(let name):
+            return "\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다."
+        case .exitProgram:
+            return "프로그램을 종료합니다..."
+        }
     }
 }
 
