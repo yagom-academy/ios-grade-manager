ecd4289af1fbbf0823648a800f06e7b2bfa266f1
john.lim <john.lim@kakaocorp.com>
2022-12-27 16:14:18 +0900

    test: 학생 추가 테스트

--- GradeManagerTests/GradeManagerTests.swift
+++ GradeManagerTests/GradeManagerTests.swift
@@ -20,4 +20,29 @@ final class GradeManagerTests: XCTestCase {
         //then
         XCTAssertEqual(result, nil)
     }
+    
+    func test_중복_학생_입력_시_오류() {
+        //given
+        let input = "dupStudent"
+        
+        //when
+        sut.addStudent(of: input)
+        
+        //then
+        let result = sut.isValid(name: input)
+        XCTAssertEqual(result, false)
+    }
+    
+    func test_미중복_학생_입력_시_통과() {
+        //given
+        let input = "Student"
+        let currentStudentCount = sut.students.count
+        
+        //when
+        sut.addStudent(of: input)
+        
+        //then
+        let result = sut.students.count
+        XCTAssertEqual(result, currentStudentCount + 1)
+    }
 }
